029f81be50834b2f4d69ceb632ceaaa78ac02d81
Christian García <christiangv99@gmail.com>
2023-01-19 22:03:49 +0000

    test changes

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -6,6 +6,19 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
+		Application.Current.UserAppTheme = AppTheme.Light;
+
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        if (window != null)
+        {
+            window.Title = "elBarto Task List";
+        }
+
+        return window;
+    }
 }
